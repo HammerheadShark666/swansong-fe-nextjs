6596ff6da97d847f707f2764692eb371b9e9f5e5
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-12-11 14:37:21 +0000

    Reset width of sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -39,37 +39,36 @@ export default function Sidebar({ isOpen, toggle, }: { isOpen: boolean, toggle: 
         </button>
 
         <div className="grid col-span-1 grid-col-12">
-
-        <ul className="sidebar-nav leading-relaxed text-left pl-10 pr-5 text-lg">
-          <li>
-            <Link href="/albums" onClick={toggle}>
-              <p>Albums</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/artists" onClick={toggle}>
-              <p>Artists</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/members" onClick={toggle}>
-              <p>Members</p>
-            </Link>
-          </li> 
-          <li>
-            <div className="md:block pt-4">                
-              <div className="relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 md:pt-0 pointer-events-none">
-                  <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                  </svg>
-                </div>
-                <input onKeyDown={handleOnKeyDown} type="search" id="default-search" className="block w-11/12 p-3 ps-10 text-sm text-gray-900 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
-              </div>   
-            </div>
-          </li>        
-        </ul>
-      </div> 
+          <ul className="sidebar-nav leading-relaxed text-left pl-10 pr-5 text-lg">
+            <li>
+              <Link href="/albums" onClick={toggle}>
+                <p>Albums</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/artists" onClick={toggle}>
+                <p>Artists</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/members" onClick={toggle}>
+                <p>Members</p>
+              </Link>
+            </li> 
+            <li>
+              <div className="md:block pt-4">                
+                <div className="relative">
+                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 md:pt-0 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                  </div>
+                  <input onKeyDown={handleOnKeyDown} type="search" id="default-search" className="block w-11/12 p-3 ps-10 text-sm text-gray-900 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
+                </div>   
+              </div>
+            </li>        
+          </ul>
+        </div> 
       </div>
     </>
   );
